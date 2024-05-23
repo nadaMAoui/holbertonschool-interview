c2d8e668b3377d6c42ea64d249552c62967d126e
nadaMAoui <maaouinada747@gmail.com>
2024-05-23 23:45:37 +0200

    betty style

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -7,16 +7,16 @@
  */
 void menger(int level)
 {
-    int x, y, size = pow(3, level);
+	int x, y, size = pow(3, level);
 
-    for (y = 0; y < size; y++)
-    {
-        for (x = 0; x < size; x++)
-        {
-            printf("%c", sponge_char(y, x));
-        }
-        printf("\n");
-    }
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			printf("%c", sponge_char(y, x));
+		}
+		printf("\n");
+	}
 }
 
 /**
@@ -27,13 +27,13 @@ void menger(int level)
  */
 char sponge_char(int y, int x)
 {
-    while (y && x)
-    {
-        if (y % 3 == 1 && x % 3 == 1)
-            return (' ');
+	while (y && x)
+	{
+		if (y % 3 == 1 && x % 3 == 1)
+			return (' ');
 
-        y /= 3;
-        x /= 3;
-    }
-    return ('#');
+		y /= 3;
+		x /= 3;
+	}
+	return ('#');
 }
