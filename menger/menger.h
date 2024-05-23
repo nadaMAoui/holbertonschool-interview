1ded0408a436b6812b8fa772a0f2b10ad71496b7
nadaMAoui <maaouinada747@gmail.com>
2024-05-23 23:16:17 +0200

    updating

---
+++ menger/menger.h
@@ -0,0 +1,11 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void menger(int level);
+char sponge_char(int y, int x);
+
+#endif /* MENGER_H */
