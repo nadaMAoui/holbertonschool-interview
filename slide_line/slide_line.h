19c2d103a97e6bb207c3cc16d9174e59b2fa2ba1
nadaMAoui <maaouinada747@gmail.com>
2024-05-23 22:35:47 +0200

    updating

--- slide_line/slide_line.h
+++ slide_line/slide_line.h
@@ -4,9 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SLIDE_LEFT 0
-#define SLIDE_RIGHT 1
+#define SLIDE_LEFT 4
+#define SLIDE_RIGHT 6
 
 int slide_line(int *line, size_t size, int direction);
+void slide_right(int *line, int size);
+void slide_left(int *line, int size);
 
 #endif
