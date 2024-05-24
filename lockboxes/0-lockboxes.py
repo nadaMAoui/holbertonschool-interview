7f7fed9f5e9fea1495607a7a07ec8f9786685407
nadaMAoui <maaouinada747@gmail.com>
2024-05-24 11:16:54 +0200

    lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that finds out if all lockboxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    if not boxes:
+        return False
+    box_len = len(boxes)
+    open_box = [0]
+
+    for key in open_box:
+        for box in boxes[key]:
+            if box not in open_box and box < box_len:
+                open_box.append(box)
+
+    if len(open_box) == box_len:
+        return True
+    return False
+    
