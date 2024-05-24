8fad12ff153087162c66dec1ca96357b93f420a4
nadaMAoui <maaouinada747@gmail.com>
2024-05-24 10:56:26 +0200

    updates

--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -1,10 +1,10 @@
+#ifndef SORT_H
+#define SORT_H
 
-#ifndef HEAP_SORT
-#define HEAP_SORT
 #include <stdlib.h>
 #include <stdio.h>
 
-void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
 
-#endif /*END ENDIF*/
+#endif
