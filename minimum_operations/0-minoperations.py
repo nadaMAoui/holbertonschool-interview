dba24c7fdb7a21c4c5737c0a979d88d0dc43d5fc
nadaMAoui <maaouinada747@gmail.com>
2024-05-28 08:55:02 +0200

    add

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,20 +1,30 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
 Module for finding the minimum number of operations to get n amount of Hs
 """
 
 
 def minOperations(n):
-    op = 0
-    number = 2
+    """
+    Function that finds le minimum number of operations to get n amount of Hs
+    """
     if type(n) is not int:
         return 0
     if n <= 1:
-        return op
-    while n > 1:
-        if n % number == 0:
-            op+= number
-            n = n / number
+        return 0
+
+    ops = 0
+    chars = 1
+    copy = 1
+
+    while chars < n:
+        if n % chars == 0:
+            copy = chars
+            ops += 1
+        if chars != n:
+            chars += copy
+            ops += 1
         else:
-            number += 1
-    return(op)
+            break
+
+    return ops
