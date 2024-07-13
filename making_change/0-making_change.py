35e42e62e68935327c4f530be9f0a3e56e7932bf
nadaMAoui <maaouinada747@gmail.com>
2024-07-13 20:41:58 +0200

    fixing output

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""Making Change module"""
+"""
+Making Change module 
+"""
 
 
 def makeChange(coins, total):
-    """Given a pile of coins of different values,
-    determine the fewest number of coins needed to meet a given amount total.
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of
     """
     if total <= 0:
         return 0
-    coins = sorted(coins)
-    coins = reversed(coins)
-    s = 0
-    for i in coins:
-        sc = total // i
-        total = total - (sc * i)
-        s += sc
-    if total == 0:
-        return s
-    return -1
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
