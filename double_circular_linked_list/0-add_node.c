da23acf740a25e1ecd31ff245ac9b792c841f8ad
nadaMAoui <maaouinada747@gmail.com>
2024-07-19 22:23:19 +0200

    double_circular ...

---
+++ double_circular_linked_list/0-add_node.c
@@ -0,0 +1,99 @@
+#include "list.h"
+
+/**
+ * add_node_end - adds a node at the end of a double circular linked list
+ * @list: the double circular linked list to insert the node into
+ * @str: the string to instert in the node
+ * Return: the address of the new insterted node or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *new = NULL, *first = *list, *last = *list;
+
+	new = malloc(sizeof(List));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	if (str && !new->str)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	while (last && last->next && last->next != first)
+		last = last->next;
+
+	if (first)
+	{
+		new->next = first;
+		first->prev = new;
+	}
+
+	else
+	{
+		*list = new;
+		new->next = new;
+	}
+
+	if (last)
+	{
+		new->prev = last;
+		last->next = new;
+	}
+
+	else
+		new->prev = new;
+
+	return (new);
+}
+
+/**
+ * add_node_begin - adds a node at the beginning of a double circular linked
+ *                  list
+ * @list: the double circular linked list to insert the node into
+ * @str: the string to instert in the node
+ * Return: the address of the new insterted node or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *new = NULL, *first = *list, *last = *list;
+
+	new = malloc(sizeof(List));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	if (str && !new->str)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	while (last && last->next && last->next != first)
+		last = last->next;
+
+	if (first)
+	{
+		new->next = first;
+		first->prev = new;
+	}
+
+	else
+		new->next = new;
+
+	if (last)
+	{
+		new->prev = last;
+		last->next = new;
+	}
+
+	else
+		new->prev = new;
+
+	*list = new;
+
+	return (new);
+}
