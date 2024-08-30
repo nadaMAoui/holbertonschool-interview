ad79dcf38ef874547c0adf6ca8842a588e3c2312
Maaoui.nada <113900693+nadaMAoui@users.noreply.github.com>
2024-08-30 22:53:26 +0200

    Update regex.c 00

--- regex/regex.c
+++ regex/regex.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
-//regex
 
-int regex_match(char const *str, char const *pattern) {
-    if (*str == '\0' && *pattern == '\0') {
+
+int regex_match(char const *str, char const *pattern) 
+{
+    if (*str == '\0' && *pattern == '\0') 
         return 1;
-    }
 
-    if (*str != '\0' && *pattern == '\0') {
+    if (*str != '\0' && *pattern == '\0') 
+    {
         return 0;
     }
 
-    if (*(pattern + 1) == '*') {
+    if (*(pattern + 1) == '*') 
+    {
         while ((*str == *pattern) || (*pattern == '.' && *str != '\0')) {
             if (regex_match(str, pattern + 2)) {
                 return 1;
             }
             str++;
-        }
+    }
         return regex_match(str, pattern + 2);
     }
 
-    if (*str == *pattern || (*pattern == '.' && *str != '\0')) {
+    if (*str == *pattern || (*pattern == '.' && *str != '\0')) 
+    {
         return regex_match(str + 1, pattern + 1);
     }
 
