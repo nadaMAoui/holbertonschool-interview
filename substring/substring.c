4f50f076c18013afe5f9a4e87dc9b1c131efde34
Maaoui.nada <113900693+nadaMAoui@users.noreply.github.com>
2024-09-05 10:49:13 +0200

    Update substring.c

--- substring/substring.c
+++ substring/substring.c
@@ -9,14 +9,14 @@
  */
 int check_in_array(int *arr, int len, int ans)
 {
-    int i = 0;
+int i = 0;
 
-    for (i = 0; i < len; i++)
-    {
-        if (arr[i] == ans)
-            return (0);
-    }
-    return (1);
+for (i = 0; i < len; i++)
+{
+if (arr[i] == ans)
+return (0);
+}
+return (1);
 }
 
 /**
@@ -27,14 +27,14 @@ int check_in_array(int *arr, int len, int ans)
  */
 int check_word(char const *str, char const *word)
 {
-    int i = 0;
+int i = 0;
 
-    for (i = 0; *(word + i) != '\0'; i++)
-    {
-        if (*(str + i) != *(word + i))
-            return (0);
-    }
-    return (1);
+for (i = 0; *(word + i) != '\0'; i++)
+{
+if (*(str + i) != *(word + i))
+return (0);
+}
+return (1);
 }
 
 /**
@@ -47,40 +47,39 @@ int check_word(char const *str, char const *word)
  */
 int match(char const *s, char const **words, int nb_words, int word_len)
 {
-    int *checked;
-    int checked_len = 0;
-    int found = 0;
-    int match = 1;
-    int i, j, k;
-    int in_array;
+int *checked;
+int checked_len = 0;
+int found = 0;
+int match = 1;
+int i, j, k;
+int in_array;
 
-    checked = malloc(sizeof(int) * nb_words);
+checked = malloc(sizeof(int) * nb_words);
 
-    for (k = 0; k < nb_words; k++)
-        checked[k] = -1;
+for (k = 0; k < nb_words; k++)
+checked[k] = -1;
 
-    for (i = 0; i < nb_words; i++)
-    {
-        found = 0;
-        for (j = 0; j < nb_words; j++)
-        {
-            in_array = check_in_array(checked, checked_len, j);
-            if (in_array && check_word((s + (i * word_len)), words[j]))
-            {
-                checked[checked_len] = j;
-                checked_len++;
-                found = 1;
-                break;
-            }
-        }
-        if (found == 0)
-        {
-            match = 0;
-            break;
-        }
-    }
-    free(checked);
-    return (match);
+for (i = 0; i < nb_words; i++)
+{
+found = 0;
+for (j = 0; j < nb_words; j++)
+{
+in_array = check_in_array(checked, checked_len, j);
+if (in_array && check_word((s + (i * word_len)), words[j]))
+{
+checked[checked_len] = j;
+checked_len++;
+found = 1;
+break;
+}
+}
+if (found == 0)
+{
+match = 0;break;
+}
+}
+free(checked);
+return (match);
 }
 
 /**
@@ -93,38 +92,38 @@ int match(char const *s, char const **words, int nb_words, int word_len)
  */
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-    int *result;
-    int str_len = 0;
-    int word_len = 0;
-    int i;
+int *result;
+int str_len = 0;
+int word_len = 0;
+int i;
 
-    *n = 0;
-    if (s == NULL || words == NULL || *words == NULL || nb_words == 0)
-        return (NULL);
+*n = 0;
+if (s == NULL || words == NULL || *words == NULL || nb_words == 0)
+return (NULL);
 
-    for (i = 0; s[i] != '\0'; i++)
-        str_len++;
-    for (i = 0; words[0][i] != '\0'; i++)
-        word_len++;
+for (i = 0; s[i] != '\0'; i++)
+str_len++;
+for (i = 0; words[0][i] != '\0'; i++)
+word_len++;
 
-    result = malloc(sizeof(int) * str_len);
-    if (result == NULL)
-        return (NULL);
+result = malloc(sizeof(int) * str_len);
+if (result == NULL)
+return (NULL);
 
-    for (i = 0; *(s + i) != '\0'; i++)
-    {
-        if (match(s + i, words, nb_words, word_len))
-        {
-            result[*n] = i;
-            *n = *n + 1;
-        }
-    }
+for (i = 0; *(s + i) != '\0'; i++)
+{
+if (match(s + i, words, nb_words, word_len))
+{
+result[*n] = i;
+*n = *n + 1;
+}
+}
 
-    if (*n == 0)
-    {
-        free(result);
-        return (NULL);
-    }
+if (*n == 0)
+{
+free(result);
+return (NULL);
+}
 
-    return (result);
+return (result);
 }
